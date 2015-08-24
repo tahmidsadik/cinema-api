8c8db8f8e8d796777d4786cd105afd009776ba4d
Tahmid Sadik <tahmidsadik112@gmail.com>
2015-08-24 07:21:15 +0600

    Data Validation and Annotations
    # Removes 3d form the name of movies.
    # :imdb-name contains proper name for querying omdb api.
    # Async requests with http-kit and core-async for faster data fetching
    from omdb.

--- src/cine/server.clj
+++ src/cine/server.clj
@@ -7,8 +7,52 @@
             [hiccup.core :as h]
             [ring.middleware.defaults :refer :all]
             [cheshire.core :as cheshire ]
+            [org.httpkit.client :as http]
+            [clojure.core.async :as a :refer [go <! >! <!! >!!]]
             [cine.core :as cine])
   (:gen-class))
+
+(defn add-3d-info [mov]
+  (if (.contains (str/lower-case (:name mov)) "3d")
+    (assoc mov :3d true)
+    (assoc mov :3d false)))
+
+(defn strip-x-from-name 
+  "Strips 'x' from name where 'x' is a string or char-sequence or regexp"
+  [mov x]
+  (str/replace mov x ""))
+
+(defn list-movies []
+  (->> (cine/mapify-current-movies)
+       (map add-3d-info)
+       (map #(assoc % :name (strip-x-from-name (:name %) "(3D)")))
+       (map #(assoc % :name (strip-x-from-name (:name %) "(2D)")))
+       (map #(assoc % :imdb-name (-> (:name %)
+                                     (str/trim)
+                                     (str/replace "-" "")
+                                     (str/replace "  " " ")
+                                     (str/replace " " "+"))))
+       ;; (map #(assoc % :imdb-name (str/replace 
+       ;;                             (str/replace 
+       ;;                               (str/trim (:name %)) "-" "") " " "+")))
+       ))
+
+(list-movies)
+
+(defn http-get [url]
+  (let [c (a/chan)]
+    (http/get url #(a/put! c %))
+    c))
+
+(time (dotimes [n 10]
+        (<!! (http-get "http://www.omdbapi.com?t=Selma"))))
+(time (let [chans (doall (for [x (list-movies)]
+                           (http-get (str "http://www.omdbapi.com/?t=" (:name x)))))]
+        (for [c chans]
+          (<!! c))))
+
+
+(time (doall (pmap #(client/get (str "http://www.omdbapi.com/?t=" (:name %))) (list-movies))))
 
 (defn current-movie-list-handler [req]
   {:status 200
