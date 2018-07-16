cf65994beae35a06be3f8c9273c41d648bf4f3d5
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-16 09:43:19 +0600

    Updates the test suite and circleci config to use postgres instead of mysql.

--- config/test.exs
+++ config/test.exs
@@ -11,8 +11,8 @@ config :logger, level: :warn
 
 # Configure your database
 config :cinema_api, CinemaApi.Repo,
-  adapter: Ecto.Adapters.MySQL,
-  username: "root",
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
   password: "",
   database: "cinema_api_test",
   hostname: "localhost",
