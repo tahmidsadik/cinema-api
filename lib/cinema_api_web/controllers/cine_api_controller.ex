d8a4521f9b541c2841904b17e271bec45cbae0d0
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-16 06:56:16 +0600

    persists the movies after fetching them.

--- lib/cinema_api_web/controllers/cine_api_controller.ex
+++ lib/cinema_api_web/controllers/cine_api_controller.ex
@@ -1,9 +1,13 @@
 defmodule CinemaApiWeb.CineApiController do
   import CinemaApi.DataProvider, only: [get_cineplex_movies_with_omdb_data: 0]
+  import CinemaApi.DataPersistence
   use CinemaApiWeb, :controller
 
   def index(conn, _params) do
-    json(conn, get_cineplex_movies_with_omdb_data())
+    movies = get_cineplex_movies_with_omdb_data()
+    save_fetched_movies(movies)
+    save_showtimes(movies)
+    json(conn, movies)
     # case get_movies_with_imdb_info() do
     #   {:ok, movies} -> json(conn, %{movies: movies})
     #   {:err, msg} -> json(conn, %{err: msg})
