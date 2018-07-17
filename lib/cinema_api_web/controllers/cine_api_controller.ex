234c10f2e58e607b3b1b8fb5938ee456425b720a
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-18 04:27:55 +0600

    Adds controller func for handling new route

--- lib/cinema_api_web/controllers/cine_api_controller.ex
+++ lib/cinema_api_web/controllers/cine_api_controller.ex
@@ -1,6 +1,7 @@
 defmodule CinemaApiWeb.CineApiController do
   import CinemaApi.DataProvider, only: [get_cineplex_movies_with_omdb_data: 0]
   import CinemaApi.DataPersistence
+  import CinemaApi.Movies, only: [get_movies: 0]
   use CinemaApiWeb, :controller
 
   def index(conn, _params) do
@@ -12,5 +13,10 @@ defmodule CinemaApiWeb.CineApiController do
     #   {:ok, movies} -> json(conn, %{movies: movies})
     #   {:err, msg} -> json(conn, %{err: msg})
     # end
+  end
+
+  def provide_cineplex_movies(conn, _params) do
+    movies = get_movies()
+    json(conn, movies)
   end
 end
