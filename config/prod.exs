a2d74bf41271b4fcc91e9c7de0eeca2d13d7426c
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-18 05:32:42 +0600

    Removes redundent import statements

--- config/prod.exs
+++ config/prod.exs
@@ -70,4 +70,4 @@ config :cinema_api, CinemaApi.Repo,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
