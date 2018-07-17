94df6cf97b293215e088a94d564d743d003b50c5
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-18 04:27:23 +0600

    Adds a new route /api/movies/cineplex

--- lib/cinema_api_web/router.ex
+++ lib/cinema_api_web/router.ex
@@ -2,28 +2,32 @@ defmodule CinemaApiWeb.Router do
   use CinemaApiWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"] end
+    plug(:accepts, ["json"])
+  end
 
   scope "/", CinemaApiWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    get "/hello", HelloController, :index
-    get "/hello/:messenger", HelloController, :show
+    get("/", PageController, :index)
+    get("/hello", HelloController, :index)
+    get("/hello/:messenger", HelloController, :show)
   end
 
   scope "/api", CinemaApiWeb do
-    pipe_through :api # User the default api stack
+    # User the default api stack
+    pipe_through(:api)
 
-    get "/", CineApiController, :index
+    get("/fetch", CineApiController, :index)
+    get("/movies/cineplex", CineApiController, :provide_cineplex_movies)
   end
 
   # Other scopes may use custom stacks.
