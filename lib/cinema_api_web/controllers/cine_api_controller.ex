f7bebfbb48ee50cdde3d104adaeb7e8f1eba6c54
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-07-20 14:14:58 +0600

    Fixes compile errors

--- lib/cinema_api_web/controllers/cine_api_controller.ex
+++ lib/cinema_api_web/controllers/cine_api_controller.ex
@@ -1,13 +1,12 @@
 defmodule CinemaApiWeb.CineApiController do
   import CinemaApi.DataProvider, only: [get_cineplex_movies_with_omdb_data: 0]
-  import CinemaApi.DataPersistence
+  import CinemaApi.DataPersistence, only: [persist_movies: 1]
   import CinemaApi.Movies, only: [get_movies: 0]
   use CinemaApiWeb, :controller
 
   def index(conn, _params) do
     movies = get_cineplex_movies_with_omdb_data()
-    save_fetched_movies(movies)
-    save_showtimes(movies)
+    persist_movies(movies)
     json(conn, movies)
     # case get_movies_with_imdb_info() do
     #   {:ok, movies} -> json(conn, %{movies: movies})
