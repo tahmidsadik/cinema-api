dbfa44cf143352eed5a652db5c18d51eb87a78ab
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-08-08 01:25:33 +0600

    Adds dummy test 😍

---
+++ test/cinema_api/data_persistence_test.exs
@@ -0,0 +1,11 @@
+defmodule CinemaApi.DataPersistenceTest do
+  @moduledoc """
+  provides functions for testing the CinemaApi.DataPersistence module
+  """
+  use ExUnit.Case
+
+  test "2 + 2 == 4" do
+    assert 2 + 2 == 4
+  end
+
+end
