54cb10ad30feaa9e728644ea5aeb83a90f34b7fd
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-12-08 18:41:40 +0600

    Fix api sending movies but only showtimes bug

--- lib/cinema_api/movies.ex
+++ lib/cinema_api/movies.ex
@@ -13,32 +13,12 @@ defmodule CinemaApi.Movies do
     movies = Repo.all(from(m in Movie, preload: [:showtimes]))
 
     movies
-    |> map(fn mov -> mov |> from_struct() end)
+    |> map(fn mov -> mov |> from_struct() |> delete(:__meta__) end)
     |> map(fn mov ->
-      mov.showtimes
-      |> map(fn st -> st |> from_struct end)
-      |> map(fn st -> st |> delete(:__meta__) end)
+      %{
+        mov
+        | showtimes: mov.showtimes |> map(fn st -> st |> from_struct() |> delete(:__meta__) end)
+      }
     end)
-
-    # movies = Repo.all(Movie)
-    # showtimes =
-    #   movies
-    #   |> filter(fn mov -> mov.imdb_id != nil end)
-    #   |> map(fn mov ->
-    #     from(
-    #       s in Showtime,
-    #       where: s.movie_id == ^mov.id,
-    #       select: %{id: s.id,  }
-    #     )
-    #   end)
-    #   |> map(fn query -> Repo.all(query) end)
-    #   |> map(fn showtime_list -> map(showtime_list, fn s -> s.showtime end) end)
-    #
-    # zip(movies, showtimes)
-    # |> map(fn movie -> Map.put(elem(movie, 0), :schedules, elem(movie, 1)) end)
-    # |> map(fn mov ->
-    #   {_, y} = Map.from_struct(mov) |> Map.pop(:__meta__)
-    #   y
-    # end)
   end
 end
