ef72bb18b095cf5dd33b0c8093437f549fd54563
Tahmid Sadik <tahmidsadik112@gmail.com>
2018-12-09 15:33:28 +0600

    Remove force ssl config

--- config/prod.exs
+++ config/prod.exs
@@ -19,8 +19,7 @@ config :cinema_api, CinemaApiWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
-  version: Application.spec(:phoenix_distillery, :vsn),
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  version: Application.spec(:phoenix_distillery, :vsn)
 
 # Do not print debug messages in production
 config :logger, level: :info
